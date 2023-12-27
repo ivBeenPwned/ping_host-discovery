33085361f4ef01fdeb1dd7b318a16b9bad06a436
ivBeenPwned <100182360+ivBeenPwned@users.noreply.github.com>
2023-12-27 13:37:39 -0300

    Create ip_discovery.sh

---
+++ ip_discovery.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# != 1 ]; then
+	printf "Usage: ..."
+else
+	for i in {1..254}; do
+		#(ping -c 1 $1.$i 2>/dev/null | grep -i "ttl" | cut -d ' ' -f4 | tr -d ':') &
+		(ping -c 1 $1.$i > /dev/null 2>&1 && echo "Host $1.$i is Up") &	
+	done	
+	wait
+fi
