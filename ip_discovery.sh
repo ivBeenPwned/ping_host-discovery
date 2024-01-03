f66dbfd979eff7bc5a58471dafed2e6b31fe6664
ivBeenPwned <100182360+ivBeenPwned@users.noreply.github.com>
2024-01-03 12:13:06 -0300

    Update ip_discovery.sh

--- ip_discovery.sh
+++ ip_discovery.sh
@@ -4,7 +4,6 @@ if [ $# != 1 ]; then
 	printf "Usage: $0 192.168.0"
 else
 	for i in {1..254}; do
-		#(ping -c 1 $1.$i 2>/dev/null | grep -i "ttl" | cut -d ' ' -f4 | tr -d ':') &
 		(ping -c 1 $1.$i > /dev/null 2>&1 && echo "Host $1.$i is Up") &	
 	done	
 	wait
